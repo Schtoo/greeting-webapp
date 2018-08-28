8bc5f8324aeb32e426293da627cb3a48f939a5fb
Schtoo <mgqobhoz@gmail.com>
2018-08-28 10:56:05 +0200

    added greetings webapp with working errors

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const greetings = require('./greetingsFactory.js');
+const flash = require('express-flash');
+const session = require('express-session');
 let greetingsInstance = greetings();
 
 app.engine('handlebars', exphbs({
@@ -18,6 +20,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(session({
+    secret : "<add a secret string here>",
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  app.use(flash());
+
 app.get('/', function (req, res) {
     res.render('home')
 })
@@ -25,6 +35,14 @@ app.get('/', function (req, res) {
 app.post('/greetings', function (req, res) {
     let name = req.body.names;
     let lang = req.body.language;
+
+    if (name === "" || name === undefined){
+        req.flash('info',  'Please enter a name')
+    } else if (lang === undefined){
+        req.flash('info', 'Please select language')
+    } else {
+        greetingsInstance.greeting(name, lang);
+    }
 
     let greeter = {
         greet: greetingsInstance.greeting(name, lang),
