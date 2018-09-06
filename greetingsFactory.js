5894d63562c7b39178d0e1253e117357f3e28d98
Schtoo <mgqobhoz@gmail.com>
2018-09-06 16:06:37 +0200

    added backend functionality

--- greetingsFactory.js
+++ greetingsFactory.js
@@ -1,47 +1,46 @@
-module.exports = function (stored) {
-        var names = stored || {};
+module.exports = function (pool) {
+  async function greeting(Name, lang) {
+      if (Name !== '' && lang !== undefined) {
+        Name = Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase()
 
-        function resetBttn (){
-          names = {};
-          counter = 0;
-          lang = '';
-          Name = '';
+        let result = await pool.query('select id from users where names=$1', [Name]);
+       // console.log(result);
+        if (result.rowCount === 0) {
+          await pool.query('INSERT into users (names, count) values ($1, 0)', [Name]);
         }
-      
-        function giveName(name) {
-          if (name) {
-            if (names[name] === undefined) {
-                names[name] = 0;
-             }
-           }
-           return name;
+        await pool.query('update users set count=count+1 where names=$1', [Name]);
+        // await pool.query('INSERT into users (name, count) values ($1, 0)', [name]);
+
+        if (lang === "English") {
+          return "Hello " + Name;
+        } else if (lang === "isiXhosa") {
+          return "Molo " + Name;
+        } else if (lang === "Afrikaans") {
+          return "Hallo " + Name;
         }
-      
-        function greeting(Name, lang) {
-          Name = Name.charAt(0).toUpperCase() + Name.slice(1);
-          if(Name !=='' && lang!==undefined){
-            giveName(Name);
-            if (lang === "English") {
-              return "Hello " + Name;
-            } else if (lang === "isiXhosa") {
-              return "Molo " + Name;
-            } else if (lang === "Afrikaans") {
-              return "Hallo " + Name;
-            }
-          }
-        }
-      
-        function counter() {
-          return Object.keys(names).length;
-        }
-      
-        function getMap() {
-          return names;
-        }
-        return {
-          greeting,
-          counter,
-          getMap,
-          resetBttn
-        }
+      }
+    }
+    
+    async function counter (counta) {
+      counta = await pool.query('SELECT * FROM users');
+      return counta.rowCount;
+    }
+
+    async function resetBttn () {
+      let resetCounta = await pool.query('DELETE FROM users');
+      lang = "";
+      Name = '';
+    }
+    async function user () {
+      let greetedUser = await pool.query('SELECT * FROM users ORDER BY count DESC');
+     // console.log(greetedUser);
+      return greetedUser.rows;
+    }
+
+  return {
+    greeting,
+    counter,
+    resetBttn,
+    user
+  }
 }
